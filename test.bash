2424ab0196d5a146ff81eb7512b464bccadcf54c
Yuto Okuda <s22C1024DH@s.chibakoudai.jp>
2023-11-02 12:16:17 +0900

    Add a test.bash

--- test.bash
+++ test.bash
@@ -11,15 +11,15 @@ res=0
 
 ### I/o TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = 15.0 ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
-["$?" = 1 ]	   || ng ${LINENO}
+[ "$?" = 1 ]	   || ng ${LINENO}
 [ "${out}" = "" ]  || ng ${LINENO}
 
 out=$(echo | ./plus)
-["$?" = 1 ]        || ng ${LINENO}
+[ "$?" = 1 ]       || ng ${LINENO}
 [ "${out}" = "" ]  || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK       # &&(AND記号)は左側が成功すると右側を実行
